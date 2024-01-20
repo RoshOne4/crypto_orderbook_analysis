20033312a55d11917b6e5300757e0af514d74e44
Vituzan <s.vituzan@gmail.com>
2024-01-20 16:21:15 +0100

    date fix

--- src/analysis.js
+++ src/analysis.js
@@ -5,9 +5,9 @@ const { pool } = require("./db");
 const formatter = Intl.NumberFormat('en', { notation: 'compact' });
 
 const calculImbalance = async () => {
-	const res = await pool.query("SELECT * FROM order_book WHERE symbol = 'SOLUSDT' ORDER BY time DESC LIMIT 1;");
+	const res = await pool.query("SELECT * FROM order_book WHERE symbol = 'SOLUSDT' ORDER BY date DESC LIMIT 1;");
 
-	const date = res.rows[0].time
+	const date = res.rows[0].date
 	console.log(date, moment(date).local().format())
 	const bids = res.rows[0].bids.slice(0, 1000);
 	const asks = res.rows[0].asks.slice(0, 1000);
@@ -16,8 +16,9 @@ const calculImbalance = async () => {
 	const totalAskQuantity = asks.reduce((total, ask) => total + parseFloat(ask[1]), 0);
 
 	// Calculate the percentage difference
-	const imbalancePercentage = Math.abs((totalBidQuantity - totalAskQuantity) / ((totalBidQuantity + totalAskQuantity) / 2)) * 100;
+	const imbalancePercentage = (totalBidQuantity - totalAskQuantity) / ((totalBidQuantity + totalAskQuantity) / 2) * 100;
 	
+	console.log(imbalancePercentage.toFixed(2))
 	if (imbalancePercentage > 50)
 		sendTelegram(`SOLUSDT imbalance ${imbalancePercentage.toFixed(2)}%\nBid: ${formatter.format(totalBidQuantity.toFixed(0))}\nAsk: ${formatter.format(totalAskQuantity.toFixed(0))}`);
 }
