9045b9b5d995b7f10f516fdd427c88361ad9ceca
Vithush SK <s.vituzan@gmail.com>
2025-10-27 03:55:00 +0100

    Remove hardcoded database password
    Removed the hardcoded password for database connection.

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ const pool = new Pool({
 	user: 'postgres',
 	host: 'database-1.cfye8ieueblq.eu-west-3.rds.amazonaws.com',
 	database: 'crypto_orderbook',
-	password: 'sophia89',
+	password: '',
 	port: 5432, // Default PostgreSQL port
 	ssl: {
 		rejectUnauthorized: false
